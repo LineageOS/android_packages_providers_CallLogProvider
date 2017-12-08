87a4755e8bff0166302777d3ddd1608d39e7a183
Paul Duffin <paulduffin@google.com>
2017-12-08 00:02:43 +0000

    Stop statically including legacy-android-test
    Statically including legacy-android-test leads to duplicate classes
    which causes build time problems (with Proguard) and runtime problems on
    older SDK versions. This change:
    * Stops statically including legacy-android-test.
    * Adds compile time dependencies on andoid.test.base, android.test.mock
      and android.test.runner where necessary.
    * Adds <uses-library android:name="android.test.runner"/> to any
      affected package to ensure that the classes that were included by
      legacy-android-test are still available at runtime. That also adds a
      dependency on android.test.base and android.test.mock.
    
    The following change descriptions were generated automatically and so
    may be a little repetitive. They are provided to give the reviewer
    enough information to check the comments match what has actually been
    changed and check the reasoning behind the changes.
    
    * tests/Android.mk
        Removed legacy-android-test from LOCAL_STATIC_JAVA_LIBRARIES
        because statically including the classes in CallLogBackupTests
        results in duplicate classes which leads to build time and compile
        time issues.
    
        Added 'android.test.base' to LOCAL_JAVA_LIBRARIES because
        CallLogBackupTests's source depends on its classes and because of
        these changes they are no longer present on the compilation path.
    
    Bug: 30188076
    Test: make checkbuild
    Change-Id: I660650fbe5deee05c24da26e941842ac3b582a08

--- tests/Android.mk
+++ tests/Android.mk
@@ -4,9 +4,9 @@ include $(CLEAR_VARS)
 # We only want this apk build for tests.
 LOCAL_MODULE_TAGS := tests
 
-LOCAL_STATIC_JAVA_LIBRARIES := mockito-target legacy-android-test
+LOCAL_STATIC_JAVA_LIBRARIES := mockito-target
 
-LOCAL_JAVA_LIBRARIES := android.test.runner
+LOCAL_JAVA_LIBRARIES := android.test.runner android.test.base
 
 # Only compile source java files in this apk.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
